40f162b3f7fb0f3bef37092a0a1596cc1a4873ff
Lance <xuguang.luo@moyougames.com>
2017-12-08 11:55:25 +0800

    static

--- homework2/firstsite/settings.py
+++ homework2/firstsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c*o-y%7()*z30l)h)w10tga53&k-go83l)jqg_-5qe$rtw_286'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/upload/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload').replace("//", "/")
